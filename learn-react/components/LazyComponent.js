33157e571134423e59c00bd84ab320f45bc24182
Alkesh Sharma <alkeshsharma@Alkeshs-MacBook-Air.local>
2025-02-01 16:50:17 +0530

    completed react swiggy project

---
+++ learn-react/components/LazyComponent.js
@@ -0,0 +1,5 @@
+const LazyComponent = () => {
+  return <h1>Lazy component Testing purpose</h1>;
+};
+
+export default LazyComponent;
---
+++ learn-react/utils/constants.js
@@ -0,0 +1,12 @@
+export const URL =
+  "https://www.swiggy.com/dapi/restaurants/search/v3?lat=23.030244474584375&lng=72.53045917390587&str=Square%20Thali&trackingId=dad7d274-5d9c-06fc-e3a1-24901e037d47&submitAction=ENTER&queryUniqueId=e6085f04-f924-c1ad-8e82-ced0a11eacb6";
+
+const tempvar = "punjabi";
+export const TEMPURL = `https://www.swiggy.com/dapi/restaurants/search/v3?lat=23.030244474584375&lng=72.53045917390587&str=${tempvar}&trackingId=dad7d274-5d9c-06fc-e3a1-24901e037d47&submitAction=ENTER&queryUniqueId=e6085f04-f924-c1ad-8e82-ced0a11eacb6`;
+
+export const RESTAURANT =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.030244474584375&lng=72.53045917390587&restaurantId=";
+export const RESTAURANT_MENU_IMAGE =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+export const IMAGE_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
